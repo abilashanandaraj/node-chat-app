03b9cde3c275f6d5f28a3b34c6b84ef121df5492
abilash <abilashanandaraj@gmail.com>
2018-10-25 08:57:18 +0530

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,13 @@
-        var socket = io();
+var socket = io();
 
-        socket.on('connect', function() {
-            console.log('Connected to Server');
-        });
+socket.on('connect', function() {
+    console.log('Connected to server');
+});
 
-        socket.on('disconnect', function() {
-            console.log('Disconnected from Server');
-        });
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
 
-        socket.on('newEmail', function(email) {
-            console.log('New email', email);
-        });
-
-        socket.on('newMesage', function(message) {
-            console.log('newMesage', message);
-        });
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+});
