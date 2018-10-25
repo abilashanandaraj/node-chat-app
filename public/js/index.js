ff0c7f03d3037907dc0d871c3bb5e1d697e16825
abilash <abilashanandaraj@gmail.com>
2018-10-25 07:25:49 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@
 
         socket.on('connect', function() {
             console.log('Connected to Server');
-
-            socket.emit('createMessage', {
-                from: 'Abilash',
-                text: 'Yup, that works for me.'
-            });
         });
 
         socket.on('disconnect', function() {
